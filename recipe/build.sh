b89cb7d6b096b1a3cbd1f5e697d3148ec906c187
Curtis Hampton <CurtLHampton@gmail.com>
2018-06-10 21:46:29 -0400

    Use folder in source section

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,3 @@ autoreconf -fi
 LIBLEPT_HEADERSDIR=$PREFIX/include ./configure --prefix=$PREFIX --with-extra-libraries=$PREFIX/lib
 LDFLAGS="-L$PREFIX/lib" CFLAGS="-I$PREFIX/include" make
 make install
-
-mv tessdata/* $PREFIX/share/tessdata
