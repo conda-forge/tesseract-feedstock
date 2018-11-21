29f83a7ed1b4495c419c88c4b6c3b1ac5bedc904
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-11-20 23:35:54 -0500

    Display the `config.log` on configure failure
    Useful for debugging the current configuration issues. Though generally
    worth keeping in the recipe long term should configuration issues appear
    again.

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,8 @@ LIBLEPT_HEADERSDIR="${PREFIX}/include" ./configure \
     --prefix="${PREFIX}" \
     --with-extra-libraries="${PREFIX}/lib" \
     CC="${CC}" \
-    CXX="${CXX}"
+    CXX="${CXX}" \
+    || { cat config.log; exit 1; }
 make -j $CPU_COUNT
 make install
 popd
