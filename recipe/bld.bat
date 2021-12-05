b2a421681020cb240bb82b27007e57ee664ef6ef
Ista Zahn <istazahn@gmail.com>
2021-12-05 07:52:42 -0500

    don't build training tools on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,6 +9,7 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D SW_BUILD=0 ^
+      -D BUILD_TRAINING_TOOLS:BOOL=OFF ^
       ..
 if errorlevel 1 exit 1
 
